def41d608aaa949c3550d91b23512230250ecdd4
shiwenfeng <496859850@qq.com>
2021-07-14 11:24:39 +0800

    20210714

---
+++ getting-started-with-shader/ShaderTest/Assets/Shader/03-矩阵转换/TestMatrix.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMatrix : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 worldPos = transform.parent.localToWorldMatrix.MultiplyPoint(transform.position);
+        Debug.Log(worldPos);
+        Debug.Log(target.worldToLocalMatrix.MultiplyPoint(worldPos));
+    }
+
+}
