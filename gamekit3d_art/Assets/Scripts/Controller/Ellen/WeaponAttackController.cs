30b666e8883228da5cbe7dd5964288b46b5a90cd
swFFFF <86754989+swFFFF@users.noreply.github.com>
2021-07-15 21:29:47 +0800

    20210715

--- gamekit3d_art/Assets/Scripts/Controller/Ellen/WeaponAttackController.cs
+++ gamekit3d_art/Assets/Scripts/Controller/Ellen/WeaponAttackController.cs
@@ -11,21 +11,26 @@ public class CheckPoint
 }
 public class WeaponAttackController : MonoBehaviour
 {
+    #region 字段
     public CheckPoint[] checkPoint;
     public Color color;
     private RaycastHit[] results = new RaycastHit[10];
     public LayerMask layerMask;
     public bool isAttack;
-    // Start is called before the first frame update
+    public int damage;
+    public GameObject myself;
+    private List<GameObject> attackList = new List<GameObject>();
+    #endregion
+
+    #region Unity生命周期
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(!isAttack)
+        if (!isAttack)
         {
             return;
         }
@@ -33,6 +38,18 @@ public class WeaponAttackController : MonoBehaviour
 
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        for (int i = 0; i < checkPoint.Length; i++)
+        {
+            Gizmos.color = color;
+            Gizmos.DrawSphere(checkPoint[i].point.position, checkPoint[i].radius);
+        }
+    }
+    #endregion
+
+
+    #region 方法
     public void StartAttack()
     {
         isAttack = true;
@@ -41,8 +58,9 @@ public class WeaponAttackController : MonoBehaviour
     public void EndAttack()
     {
         isAttack = false;
+        attackList.Clear();
     }
-    
+
     //检测敌人
     public void CheckGameobject()
     {
@@ -56,19 +74,33 @@ public class WeaponAttackController : MonoBehaviour
             }
         }
     }
-    
+
     //造成伤害
     public void CheckDamage(GameObject obj)
     {
-        //判断游戏物体是不是可以受伤   
-    }
+        //判断游戏物体是不是可以受伤
+        Damageable damageable = obj.GetComponent<Damageable>();
+        if (damageable == null) { return; }
 
-    private void OnDrawGizmosSelected()
-    {
-        for(int i =0; i <checkPoint.Length; i++)
+        //检测到自己
+        if (obj == myself)
         {
-            Gizmos.color = color;
-            Gizmos.DrawSphere(checkPoint[i].point.position, checkPoint[i].radius);
+            return;
         }
+
+        if (attackList.Contains(obj))
+        {
+            return;
+        }
+        //进行攻击
+        DamageMessage data = new DamageMessage();
+        data.damage = damage;
+        damageable.OnDamage(data);
+
+        attackList.Add(obj);
     }
+    #endregion
+
+
+
 }
