99a4779cf6f4d524b63514245adb6e041869ab3d
shiwenfeng <496859850@qq.com>
2021-07-20 20:03:26 +0800

    20210720

---
+++ asset-bundle/Assets/Scripts/Editor/TestAssetBundle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class TestAssetBundle : MonoBehaviour
+{
+    [MenuItem("AssetBundle/Build Windows")]
+    static void BuildAssetBundle()
+    {
+        //Debug.Log(Application.streamingAssetsPath);
+        string outputPath = Application.streamingAssetsPath;
+        if (!Directory.Exists(outputPath))
+        {
+            Directory.CreateDirectory(outputPath);
+        }
+        BuildPipeline.BuildAssetBundles(outputPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
